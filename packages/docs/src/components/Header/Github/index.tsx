f50fd1df64b4b083cf26efb02b2e6f1d12d945da
白熱 <sonne@asaki.me>
2023-12-19 16:32:02 +0800

    fix: update homepage

--- packages/docs/src/components/Header/Github/index.tsx
+++ packages/docs/src/components/Header/Github/index.tsx
@@ -4,14 +4,18 @@ import StarIcon from '@/components/Icons/Star'
 import styles from './index.module.less'
 
 export const Github = () => {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState('　')
 
 
   useEffect(() => {
     fetch('https://api.github.com/repos/dream-num/univer')
       .then(resp => resp.json())
       .then(data => {
-        setCount(data.stargazers_count)
+        const count = data.stargazers_count > 1000
+          ? `${(data.stargazers_count / 1000).toFixed(1)}k`
+          : data.stargazers_count
+
+        setCount(count)
       })
   })
 
