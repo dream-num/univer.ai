7e41b84e08016c210652e534c9411a9e1b1a8e62
Wenzhao Hu <wzhudev@gmail.com>
2024-01-02 19:54:35 +0800

    docs: add facade docs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,94 +1,116 @@
-import { defineConfig } from 'astro/config'
-import starlight from '@astrojs/starlight'
+import { defineConfig } from "astro/config";
+import starlight from "@astrojs/starlight";
 
-import react from "@astrojs/react"
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [
     starlight({
-      title: 'Univer Docs',
+      title: "Univer Docs",
       logo: {
-        light: './src/assets/logo-dark.svg',
-        dark: './src/assets/logo-light.svg'
+        light: "./src/assets/logo-dark.svg",
+        dark: "./src/assets/logo-light.svg",
       },
       social: {
-        github: 'https://github.com/dream-num/univer',
-        discord: 'https://discord.gg/z3NKNT6D2f'
+        github: "https://github.com/dream-num/univer",
+        discord: "https://discord.gg/z3NKNT6D2f",
       },
-      customCss: ['./src/styles/starlight.css'],
-      defaultLocale: 'root',
+      customCss: ["./src/styles/starlight.css"],
+      defaultLocale: "root",
       locales: {
         root: {
-          label: '简体中文',
-          lang: 'zh-CN'
+          label: "简体中文",
+          lang: "zh-CN",
         },
-        'en-us': {
-          label: 'English',
-          lang: 'en-US'
+        "en-us": {
+          label: "English",
+          lang: "en-US",
         },
-        'ja-jp': {
-          label: '日本語',
-          lang: 'ja-JP'
-        }
+        "ja-jp": {
+          label: "日本語",
+          lang: "ja-JP",
+        },
       },
-      sidebar: [{
-        label: '🔰 指南',
-        autogenerate: {
-          directory: 'guides/*.md'
+      sidebar: [
+        {
+          label: "🔰 指南",
+          autogenerate: {
+            directory: "guides/*.md",
+          },
+          items: [
+            {
+              label: "Univer 介绍",
+              link: "guides/introduction",
+            },
+            {
+              label: "快速上手",
+              autogenerate: {
+                directory: "guides/quick-start",
+              },
+            },
+            {
+              label: "简单使用 Univer",
+              autogenerate: {
+                directory: "guides/facade",
+              },
+            },
+            {
+              label: "功能一览",
+              link: "guides/features",
+            },
+            {
+              label: "其他发行版",
+              link: "guides/release",
+            },
+            {
+              label: "FAQ",
+              link: "guides/faq",
+            },
+            {
+              label: "路线图",
+              link: "guides/roadmap",
+            },
+            {
+              label: "贡献指南",
+              link: "guides/contributing",
+            },
+            {
+              label: "架构",
+              autogenerate: {
+                directory: "guides/architecture",
+              },
+            },
+            {
+              label: "插件",
+              autogenerate: {
+                directory: "guides/plugins",
+              },
+            },
+            {
+              label: "扩展",
+              autogenerate: {
+                directory: "guides/extend",
+              },
+            },
+          ],
         },
-        items: [{
-          label: 'Univer 介绍',
-          link: 'guides/introduction'
-        }, {
-          label: '快速上手',
+        {
+          label: "💼 企业服务",
           autogenerate: {
-            directory: 'guides/quick-start'
-          }
-        }, {
-          label: '功能一览',
-          link: 'guides/features'
-        }, {
-          label: '其他发行版',
-          link: 'guides/release'
-        }, {
-          label: 'FAQ',
-          link: 'guides/faq'
-        }, {
-          label: '路线图',
-          link: 'guides/roadmap'
-        }, {
-          label: '贡献指南',
-          link: 'guides/contributing'
-        }, {
-          label: '架构',
-          autogenerate: {
-            directory: 'guides/architecture'
-          }
-        }, {
-          label: '插件',
-          autogenerate: {
-            directory: 'guides/plugins'
-          }
-        }, {
-          label: '扩展',
-          autogenerate: {
-            directory: 'guides/extend'
-          }
-        }]
-      }, {
-        label: '💼 企业服务',
-        autogenerate: {
-          directory: 'enterprises'
-        }
-      }, {
-        label: '🔌 API reference',
-        link: '../api'
-      }, {
-        label: '🧩 Playground',
-        link: '../playground'
-      }]
+            directory: "enterprises",
+          },
+        },
+        {
+          label: "🔌 API Reference",
+          link: "../api",
+        },
+        {
+          label: "🧩 Playground",
+          link: "../playground",
+        },
+      ],
     }),
-    react()
-  ]
-})
+    react(),
+  ],
+});
--- src/content/docs/api.md
+++ src/content/docs/api.md
@@ -1,21 +1,22 @@
 ---
-title: 🔌 API reference
+title: 🔌 API Reference
 ---
 
-| Package |
-| --- |
-| [@univerjs/core](/api/core) |
-| [@univerjs/design](/api/design) |
-| [@univerjs/docs](/api/docs) |
-| [@univerjs/docs-ui](/api/docs-ui) |
+| Package                                         |
+| ----------------------------------------------- |
+| [@univerjs/core](/api/core)                     |
+| [@univerjs/design](/api/design)                 |
+| [@univerjs/docs](/api/docs)                     |
+| [@univerjs/docs-ui](/api/docs-ui)               |
 | [@univerjs/engine-formula](/api/engine-formula) |
-| [@univerjs/engine-numfmt](/api/engine-numfmt) |
-| [@univerjs/engine-render](/api/engine-render) |
-| [@univerjs/network](/api/network) |
-| [@univerjs/rpc](/api/rpc) |
-| [@univerjs/sheets](/api/sheets) |
+| [@univerjs/engine-numfmt](/api/engine-numfmt)   |
+| [@univerjs/engine-render](/api/engine-render)   |
+| [@univerjs/facade](/api/facade)                 |
+| [@univerjs/network](/api/network)               |
+| [@univerjs/rpc](/api/rpc)                       |
+| [@univerjs/sheets](/api/sheets)                 |
 | [@univerjs/sheets-formula](/api/sheets-formula) |
-| [@univerjs/sheets-numfmt](/api/sheets-numfmt) |
-| [@univerjs/sheets-ui](/api/sheets-ui) |
-| [@univerjs/ui](/api/ui) |
-| [@univerjs/uniscript](/api/uniscript) |
+| [@univerjs/sheets-numfmt](/api/sheets-numfmt)   |
+| [@univerjs/sheets-ui](/api/sheets-ui)           |
+| [@univerjs/ui](/api/ui)                         |
+| [@univerjs/uniscript](/api/uniscript)           |
