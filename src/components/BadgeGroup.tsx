1726f087b4feb7256f518509cd244c824aa34ed5
白熱 <sonne@asaki.me>
2024-06-27 14:48:11 +0800

    fix: fix lint

--- src/components/BadgeGroup.tsx
+++ src/components/BadgeGroup.tsx
@@ -1,5 +1,5 @@
 import { useRouter } from 'next/router'
-import { useCallback, useMemo } from 'react'
+import { useCallback } from 'react'
 import { Badge } from '@radix-ui/themes'
 
 export enum UniverTypes {
