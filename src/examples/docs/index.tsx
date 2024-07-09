3795c0faea01ca52bc59a3c49d722d3e8b8a436d
Dushusir <1414556676@qq.com>
2024-07-09 17:36:35 +0800

    feat(demo): doc demo add link

--- src/examples/docs/index.tsx
+++ src/examples/docs/index.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { LocaleType, Univer, UniverInstanceType } from '@univerjs/core'
+import { LocaleType, Tools, Univer, UniverInstanceType } from '@univerjs/core'
 import { defaultTheme } from '@univerjs/design'
 import { UniverDocsPlugin } from '@univerjs/docs'
 import { UniverDocsUIPlugin } from '@univerjs/docs-ui'
@@ -8,6 +8,22 @@ import { UniverDocsUIPlugin } from '@univerjs/docs-ui'
 import { UniverFormulaEnginePlugin } from '@univerjs/engine-formula'
 import { UniverRenderEnginePlugin } from '@univerjs/engine-render'
 import { UniverUIPlugin } from '@univerjs/ui'
+import { DocHyperLinkModel, UniverDocsHyperLinkPlugin } from '@univerjs/docs-hyper-link'
+import { UniverDocsHyperLinkUIPlugin } from '@univerjs/docs-hyper-link-ui'
+
+import { UniverDrawingPlugin } from '@univerjs/drawing'
+import { UniverDrawingUIPlugin } from '@univerjs/drawing-ui'
+import { UniverDocsDrawingPlugin } from '@univerjs/docs-drawing'
+import { UniverDocsDrawingUIPlugin } from '@univerjs/docs-drawing-ui'
+
+import DocsHyperLinkUIZhCN from '@univerjs/docs-hyper-link-ui/lib/locale/zh-CN.json'
+import DocsHyperLinkUIEnUS from '@univerjs/docs-hyper-link-ui/lib/locale/en-US.json'
+import DrawingUIZhCN from '@univerjs/drawing-ui/lib/locale/zh-CN.json'
+import DrawingUIEnUS from '@univerjs/drawing-ui/lib/locale/en-US.json'
+
+import DocsDrawingUIZhCN from '@univerjs/docs-drawing-ui/lib/locale/zh-CN.json'
+import DocsDrawingUIEnUS from '@univerjs/docs-drawing-ui/lib/locale/en-US.json'
+
 import { useEffect, useRef } from 'react'
 import { enUS, zhCN } from 'univer:locales'
 import { ALL_FEATURES_DOCUMENT_DATA } from './data'
@@ -27,8 +43,18 @@ export default function App(props: IProps) {
       theme: defaultTheme,
       locale: locale.replace('-', '') as LocaleType,
       locales: {
-        [LocaleType.EN_US]: enUS,
-        [LocaleType.ZH_CN]: zhCN,
+        [LocaleType.EN_US]: Tools.deepMerge(
+          enUS,
+          DocsHyperLinkUIEnUS,
+          DrawingUIEnUS,
+          DocsDrawingUIEnUS,
+        ),
+        [LocaleType.ZH_CN]: Tools.deepMerge(
+          zhCN,
+          DocsHyperLinkUIZhCN,
+          DrawingUIZhCN,
+          DocsDrawingUIZhCN,
+        ),
       },
     })
 
@@ -41,12 +67,25 @@ export default function App(props: IProps) {
     })
     univer.registerPlugin(UniverDocsPlugin)
     univer.registerPlugin(UniverDocsUIPlugin)
+
+    // HyperLink Plugin
+    univer.registerPlugin(UniverDocsHyperLinkPlugin)
+    univer.registerPlugin(UniverDocsHyperLinkUIPlugin)
+
+    // Floating Drawing Plugin
+    univer.registerPlugin(UniverDrawingPlugin)
+    univer.registerPlugin(UniverDrawingUIPlugin)
+    univer.registerPlugin(UniverDocsDrawingPlugin)
+    univer.registerPlugin(UniverDocsDrawingUIPlugin)
+
     // create univer sheet instance
     univer.createUnit(UniverInstanceType.UNIVER_DOC, ALL_FEATURES_DOCUMENT_DATA)
 
     univer.registerPlugin(GithubPlugin, {
       link: '/src/examples/docs',
     })
+    window.univer = univer
+    window.DocHyperLinkModel = univer.__getInjector().get(DocHyperLinkModel)
   }, [])
 
   return (
