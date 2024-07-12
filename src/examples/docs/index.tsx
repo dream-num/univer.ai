e6502b26f1b5fe320c67856ad155c92b05a533c9
Dushusir <1414556676@qq.com>
2024-07-12 21:04:58 +0800

    feat(doc): add image and comment

--- src/examples/docs/index.tsx
+++ src/examples/docs/index.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { LocaleType, Tools, Univer, UniverInstanceType } from '@univerjs/core'
+import { LocaleType, Univer, UniverInstanceType, UserManagerService } from '@univerjs/core'
 import { defaultTheme } from '@univerjs/design'
 import { UniverDocsPlugin } from '@univerjs/docs'
 import { UniverDocsUIPlugin } from '@univerjs/docs-ui'
@@ -8,21 +8,9 @@ import { UniverDocsUIPlugin } from '@univerjs/docs-ui'
 import { UniverFormulaEnginePlugin } from '@univerjs/engine-formula'
 import { UniverRenderEnginePlugin } from '@univerjs/engine-render'
 import { UniverUIPlugin } from '@univerjs/ui'
-import { DocHyperLinkModel, UniverDocsHyperLinkPlugin } from '@univerjs/docs-hyper-link'
 import { UniverDocsHyperLinkUIPlugin } from '@univerjs/docs-hyper-link-ui'
-
-import { UniverDrawingPlugin } from '@univerjs/drawing'
-import { UniverDrawingUIPlugin } from '@univerjs/drawing-ui'
-import { UniverDocsDrawingPlugin } from '@univerjs/docs-drawing'
 import { UniverDocsDrawingUIPlugin } from '@univerjs/docs-drawing-ui'
-
-import DocsHyperLinkUIZhCN from '@univerjs/docs-hyper-link-ui/lib/locale/zh-CN.json'
-import DocsHyperLinkUIEnUS from '@univerjs/docs-hyper-link-ui/lib/locale/en-US.json'
-import DrawingUIZhCN from '@univerjs/drawing-ui/lib/locale/zh-CN.json'
-import DrawingUIEnUS from '@univerjs/drawing-ui/lib/locale/en-US.json'
-
-import DocsDrawingUIZhCN from '@univerjs/docs-drawing-ui/lib/locale/zh-CN.json'
-import DocsDrawingUIEnUS from '@univerjs/docs-drawing-ui/lib/locale/en-US.json'
+import { UniverDocsThreadCommentUIPlugin } from '@univerjs/docs-thread-comment-ui'
 
 import { useEffect, useRef } from 'react'
 import { enUS, zhCN } from 'univer:locales'
@@ -43,18 +31,8 @@ export default function App(props: IProps) {
       theme: defaultTheme,
       locale: locale.replace('-', '') as LocaleType,
       locales: {
-        [LocaleType.EN_US]: Tools.deepMerge(
-          enUS,
-          DocsHyperLinkUIEnUS,
-          DrawingUIEnUS,
-          DocsDrawingUIEnUS,
-        ),
-        [LocaleType.ZH_CN]: Tools.deepMerge(
-          zhCN,
-          DocsHyperLinkUIZhCN,
-          DrawingUIZhCN,
-          DocsDrawingUIZhCN,
-        ),
+        [LocaleType.EN_US]: enUS,
+        [LocaleType.ZH_CN]: zhCN,
       },
     })
 
@@ -69,14 +47,13 @@ export default function App(props: IProps) {
     univer.registerPlugin(UniverDocsUIPlugin)
 
     // HyperLink Plugin
-    univer.registerPlugin(UniverDocsHyperLinkPlugin)
     univer.registerPlugin(UniverDocsHyperLinkUIPlugin)
 
     // Floating Drawing Plugin
-    univer.registerPlugin(UniverDrawingPlugin)
-    univer.registerPlugin(UniverDrawingUIPlugin)
-    univer.registerPlugin(UniverDocsDrawingPlugin)
     univer.registerPlugin(UniverDocsDrawingUIPlugin)
+
+    // Thread Comment Plugin
+    univer.registerPlugin(UniverDocsThreadCommentUIPlugin)
 
     // create univer sheet instance
     univer.createUnit(UniverInstanceType.UNIVER_DOC, ALL_FEATURES_DOCUMENT_DATA)
@@ -84,8 +61,18 @@ export default function App(props: IProps) {
     univer.registerPlugin(GithubPlugin, {
       link: '/src/examples/docs',
     })
-    window.univer = univer
-    window.DocHyperLinkModel = univer.__getInjector().get(DocHyperLinkModel)
+
+    const injector = univer.__getInjector()
+    const userManagerService = injector.get(UserManagerService)
+
+    const mockUser = {
+      userID: 'Owner_qxVnhPbQ',
+      name: 'Owner',
+      avatar: 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABgAAAAYCAYAAADgdz34AAAACXBIWXMAAAsTAAALEwEAmpwYAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAInSURBVHgBtZU9TxtBEIbfWRzFSIdkikhBSqRQkJqkCKTCFkqVInSUSaT0wC8w/gXxD4gU2nRJkXQWhAZowDUUWKIwEgWWbEEB3mVmx3dn4DA2nB/ppNuPeWd29mMIPXDr+RxwtgRHeW6+guNPRxogqnL7Dwz9psJ27S4NShaeZTH3kwXy6I81dlRKcmRui88swdq9AcSFL7Buz1Vmlns64MiLsCjzwnIYHLH57tbfFbs7KRaXyEU8FVZofqccOfA5l7Q8LPIkGrwnb2RPNEXWFVMUF3L+kDCk0btDDAMzOm5YfAHDwp4tG74wnzAsiOYMnJ3GoDybA7IT98/jm5+JNnfiIzAS6LlqHQBN/i6b2t/cV1Hh6BfwYlHnHP4AXi5q/8kmMMpOs8+BixZw/Fd6xUEHEbnkgclvQP2fGp7uShRKnQ3G32rkjV1th8JhIGG7tR/JyjGteSOZELwGMmNqIIigRCLRh2OZIE6BjItdd7pCW6Uhm1zzkUtungSxwEUzNpQ+GQumtH1ej1MqgmNT6vwmhCq5yuwq56EYTbgeQUz3yvrpV1b4ok3nYJ+eYhgYmjRUqErx2EDq0Fr8FhG++iqVGqxlUJI/70Ar0UgJaWHj6hYVHJrfKssAHot1JfqwE9WVWzXZVd5z2Ws/4PnmtEjkXeKJDvxUecLbWOXH/DP6QQ4J72NS0adedp1aseBfXP8odlZFfPvBF7SN/8hky1TYuPOAXAEipMx15u5ToAAAAABJRU5ErkJggg==',
+      anonymous: false,
+      canBindAnonymous: false,
+    }
+    userManagerService.setCurrentUser(mockUser)
   }, [])
 
   return (
