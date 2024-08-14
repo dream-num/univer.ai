006d210d81d45bc7ed829abfc94c652d19076edb
Dushusir <1414556676@qq.com>
2024-08-14 19:22:48 +0800

    feat(demo): doc demo add link (#197)

--- src/examples/docs/index.tsx
+++ src/examples/docs/index.tsx
@@ -1,6 +1,8 @@
 'use client'
+import '@univerjs/thread-comment-ui/lib/index.css'
+import '@univerjs/drawing-ui/lib/index.css'
 
-import { LocaleType, Univer, UniverInstanceType } from '@univerjs/core'
+import { LocaleType, Univer, UniverInstanceType, UserManagerService } from '@univerjs/core'
 import { defaultTheme } from '@univerjs/design'
 import { UniverDocsPlugin } from '@univerjs/docs'
 import { UniverDocsUIPlugin } from '@univerjs/docs-ui'
@@ -8,6 +10,10 @@ import { UniverDocsUIPlugin } from '@univerjs/docs-ui'
 import { UniverFormulaEnginePlugin } from '@univerjs/engine-formula'
 import { UniverRenderEnginePlugin } from '@univerjs/engine-render'
 import { UniverUIPlugin } from '@univerjs/ui'
+import { UniverDocsHyperLinkUIPlugin } from '@univerjs/docs-hyper-link-ui'
+import { UniverDocsDrawingUIPlugin } from '@univerjs/docs-drawing-ui'
+import { UniverDocsThreadCommentUIPlugin } from '@univerjs/docs-thread-comment-ui'
+
 import { useEffect, useRef } from 'react'
 import { enUS, zhCN } from 'univer:locales'
 import { ALL_FEATURES_DOCUMENT_DATA } from './data'
@@ -41,12 +47,37 @@ export default function App(props: IProps) {
     })
     univer.registerPlugin(UniverDocsPlugin)
     univer.registerPlugin(UniverDocsUIPlugin)
+
+    // HyperLink Plugin
+    univer.registerPlugin(UniverDocsHyperLinkUIPlugin)
+
+    // Floating Drawing Plugin
+    univer.registerPlugin(UniverDocsDrawingUIPlugin)
+
+    // Thread Comment Plugin
+    univer.registerPlugin(UniverDocsThreadCommentUIPlugin)
+    // univer.registerPlugin(UniverDocsMentionUIPlugin);
+
     // create univer sheet instance
     univer.createUnit(UniverInstanceType.UNIVER_DOC, ALL_FEATURES_DOCUMENT_DATA)
 
     univer.registerPlugin(GithubPlugin, {
       link: '/src/examples/docs',
     })
+
+    window.univer = univer
+
+    const injector = univer.__getInjector()
+    const userManagerService = injector.get(UserManagerService)
+
+    const mockUser = {
+      userID: 'Owner_qxVnhPbQ',
+      name: 'Owner',
+      avatar: 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABgAAAAYCAYAAADgdz34AAAACXBIWXMAAAsTAAALEwEAmpwYAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAInSURBVHgBtZU9TxtBEIbfWRzFSIdkikhBSqRQkJqkCKTCFkqVInSUSaT0wC8w/gXxD4gU2nRJkXQWhAZowDUUWKIwEgWWbEEB3mVmx3dn4DA2nB/ppNuPeWd29mMIPXDr+RxwtgRHeW6+guNPRxogqnL7Dwz9psJ27S4NShaeZTH3kwXy6I81dlRKcmRui88swdq9AcSFL7Buz1Vmlns64MiLsCjzwnIYHLH57tbfFbs7KRaXyEU8FVZofqccOfA5l7Q8LPIkGrwnb2RPNEXWFVMUF3L+kDCk0btDDAMzOm5YfAHDwp4tG74wnzAsiOYMnJ3GoDybA7IT98/jm5+JNnfiIzAS6LlqHQBN/i6b2t/cV1Hh6BfwYlHnHP4AXi5q/8kmMMpOs8+BixZw/Fd6xUEHEbnkgclvQP2fGp7uShRKnQ3G32rkjV1th8JhIGG7tR/JyjGteSOZELwGMmNqIIigRCLRh2OZIE6BjItdd7pCW6Uhm1zzkUtungSxwEUzNpQ+GQumtH1ej1MqgmNT6vwmhCq5yuwq56EYTbgeQUz3yvrpV1b4ok3nYJ+eYhgYmjRUqErx2EDq0Fr8FhG++iqVGqxlUJI/70Ar0UgJaWHj6hYVHJrfKssAHot1JfqwE9WVWzXZVd5z2Ws/4PnmtEjkXeKJDvxUecLbWOXH/DP6QQ4J72NS0adedp1aseBfXP8odlZFfPvBF7SN/8hky1TYuPOAXAEipMx15u5ToAAAAABJRU5ErkJggg==',
+      anonymous: false,
+      canBindAnonymous: false,
+    }
+    userManagerService.setCurrentUser(mockUser)
   }, [])
 
   return (
